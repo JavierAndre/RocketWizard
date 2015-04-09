49a1f81e4e8303580d7d9181698f9fc9dae2e345
JavierKazag <javier@javierandre.com>
2015-04-08 19:24:37 -0700

    Changed controller addNoseCone().

--- RocketWizard/src/rocketWizard/Controller.java
+++ RocketWizard/src/rocketWizard/Controller.java
@@ -39,9 +39,46 @@ public class Controller {
 	 * 
 	 */
 	
-	public boolean addNoseCone(List<SQLDBColModel> model) {
+	public boolean addNoseCone(NoseConeModel model) {
 		
-		return sqlDBModel.add(SQLDBModel.NOSE_CONES_TABLE, model);
+		List<SQLDBColModel> list = new LinkedList<SQLDBColModel>();
+		
+		SQLDBColModel colModel = new SQLDBColModel(SQLDBModel.NOSE_CONE_MANUFACTURER_ID, model.getManufacturerID());
+		list.add(colModel);
+		colModel = new SQLDBColModel(SQLDBModel.NOSE_CONE_PART_NUMBER, model.getPartNumber());
+		list.add(colModel);
+		colModel = new SQLDBColModel(SQLDBModel.NOSE_CONE_MODEL_NAME, model.getModelName());
+		list.add(colModel);
+		colModel = new SQLDBColModel(SQLDBModel.NOSE_CONE_INNER_DIAMETER_ENGLISH, model.getInnerDiameterEnglish());
+		list.add(colModel);
+		colModel = new SQLDBColModel(SQLDBModel.NOSE_CONE_OUTER_DIAMETER_ENGLISH, model.getOuterDiameterEnglish());
+		list.add(colModel);
+		colModel = new SQLDBColModel(SQLDBModel.NOSE_CONE_NOSE_LENGTH_ENGLISH, model.getNoseLengthEnglish());
+		list.add(colModel);
+		colModel = new SQLDBColModel(SQLDBModel.NOSE_CONE_SHOULDER_LENGTH_ENGLISH, model.getShoulderLengthEnglish());
+		list.add(colModel);
+		colModel = new SQLDBColModel(SQLDBModel.NOSE_CONE_WEIGHT_ENGLISH, model.getWeightEnglish());
+		list.add(colModel);
+		colModel = new SQLDBColModel(SQLDBModel.NOSE_CONE_INNER_DIAMETER_METRIC, model.getInnerDiameterMetric());
+		list.add(colModel);
+		colModel = new SQLDBColModel(SQLDBModel.NOSE_CONE_OUTER_DIAMETER_METRIC, model.getOuterDiameterMetric());
+		list.add(colModel);
+		colModel = new SQLDBColModel(SQLDBModel.NOSE_CONE_NOSE_LENGTH_METRIC, model.getNoseLengthMetric());
+		list.add(colModel);
+		colModel = new SQLDBColModel(SQLDBModel.NOSE_CONE_SHOULDER_LENGTH_METRIC, model.getShoulderLengthMetric());
+		list.add(colModel);
+		colModel = new SQLDBColModel(SQLDBModel.NOSE_CONE_WEIGHT_METRIC, model.getWeightMetric());
+		list.add(colModel);
+		colModel = new SQLDBColModel(SQLDBModel.NOSE_CONE_MATERIAL_ID, model.getMaterialID());
+		list.add(colModel);
+		colModel = new SQLDBColModel(SQLDBModel.NOSE_CONE_DATE_ADDED, model.getDateAdded());
+		list.add(colModel);
+		colModel = new SQLDBColModel(SQLDBModel.NOSE_CONE_DATE_LAST_UPDATED, model.getDateLastUpdated());
+		list.add(colModel);
+		colModel = new SQLDBColModel(SQLDBModel.NOSE_CONE_ACTIVE, (model.isActive()? 1 : 0));
+		list.add(colModel);
+		
+		return sqlDBModel.add(SQLDBModel.NOSE_CONES_TABLE, list);
 	}
 	
 	public LinkedList<NoseConeModel> getNoseCones() {
